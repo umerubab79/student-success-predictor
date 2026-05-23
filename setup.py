eab8a9cee158763759f2431743a004b7ffb5117a
Ume rubab <umerubab5789@gmail.com>
2026-05-23 21:15:56 +0500

    setup script with comments

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 from setuptools import find_packages, setup
 
-
 setup(
-    name = "Result_Analysis",
-    version="0.1.0",
-    author="ume rubab",
+    name="student_success_predictor",   
+    version="0.1.0",                    
+    author="Ume Rubab",            
     author_email="umerubab5789@gmail.com",
+    description="A machine learning project to predict student success based on performance data", 
+    long_description=open("README.md").read(),   
+    long_description_content_type="text/markdown",
+    url="https://github.com/umerubab79/student-success-predictor", 
     packages=find_packages(),
-    install_requires=[]
-    
-
-
+    install_requires=[
+        "pandas",
+        "numpy",
+        "scikit-learn",
+        "matplotlib",
+        "seaborn",
+        "flask"
+    ],  
+    classifiers=[                      
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",            
 )
